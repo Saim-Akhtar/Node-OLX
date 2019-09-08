39999c1e10535dc029ee6fd3ef41a25ea30ee8b3
Bilal <bilaljamoot1995@gmail.com>
2019-09-07 23:29:44 -0700

    Add files via upload

---
+++ front-end/js/myjs.js
@@ -0,0 +1,14 @@
+
+// Image gallary
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true
+  });
+
+  // Tabs
+
+  $(document).ready(function(){
+    $('.tabs').tabs();
+  });
+ 
+        
