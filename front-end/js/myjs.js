1db194449f45ab33b8040a293309021ebf080f7e
Bilal <bilaljamoot1995@gmail.com>
2019-09-15 03:10:48 -0700

    Add files via upload

--- front-end/js/myjs.js
+++ front-end/js/myjs.js
@@ -16,4 +16,24 @@
  $(document).ready(function(){
   $('.modal').modal();
 });
-        
+       
+
+// adding and removing card class 
+
+$(document).ready(function($) {
+  var alterClass = function() {
+    var ww = document.body.clientWidth;
+    if (ww < 780) {
+      $('#my-card').removeClass('horizontal');
+    } else if (ww >= 780) {
+      $('#my-card').addClass('horizontal');
+    };
+  };
+  $(window).resize(function(){
+    alterClass();
+  });
+  alterClass();
+});
+
+
+// My way of doing it 
